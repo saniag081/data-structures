9a9ce500fba735e3655c13e99b638e85e8831254
saniag081 <sgonzalezhernandez081@gmail.com>
2021-09-23 21:55:13 -0500

    add linkedList class

---
+++ src/LinkedList/singly.js
@@ -0,0 +1,37 @@
+1 -- > 2 -- > 3 -- > 4 -- > 5 -- > null
+
+let singlyLinkedList = {
+	head: {
+		value: 1,
+		next: {
+			value: 2,
+			next: {
+				value: 3,
+				next: {
+					value: 4,
+					next: null
+				}
+			}
+		}
+	}
+}
+
+class node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class MySinglyLinkedList {
+	constructor(value){
+		this.head = {
+			value,
+			next: null
+		}
+		this.tail = this.head;
+		this.length = 1;
+	}
+}
+
+let myLinkedList = new MySinglyLinkedList
